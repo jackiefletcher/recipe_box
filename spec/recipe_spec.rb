883582daac2856decb84c7c4f2b976987e08c97d
Jackie Fletcher & Lauren Marcus <student@epicodus.com>
2015-01-29 10:45:22 -0800

    Validate the presence of a name of a recipe.

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -2,4 +2,8 @@ require('spec_helper')
 
 describe(Recipe) do
   it { should have_and_belong_to_many (:ingredients) }
+  it("validate presence of name") do
+    recipe = Recipe.new({:name => ''})
+    expect(recipe.save()).to(eq(false))
+  end
 end
