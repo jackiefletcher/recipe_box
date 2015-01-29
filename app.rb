a9ac971f157e956e71bd1a2b98c74897873c1214
Jackie Fletcher & Lauren Marcus <student@epicodus.com>
2015-01-29 13:34:16 -0800

    Allow user to link recipes and ingredients.

--- app.rb
+++ app.rb
@@ -33,3 +33,33 @@ post('/ingredients') do
   @ingredients = Ingredient.all()
   erb(:ingredient)
 end
+
+get("/recipes/:id") do
+  @ingredients = Ingredient.all()
+  @recipe = Recipe.find(params.fetch("id").to_i())
+  erb(:recipe_info)
+end
+
+patch("/recipes/:id") do
+  recipe_id = params.fetch("id").to_i()
+  @recipe = Recipe.find(recipe_id)
+  ingredient_ids = params.fetch("ingredient_ids")
+  @recipe.update({:ingredient_ids => ingredient_ids})
+  @ingredients = Ingredient.all()
+  erb(:recipe_info)
+end
+
+get("/ingredients/:id") do
+  @recipes = Recipe.all()
+  @ingredient = Ingredient.find(params.fetch("id").to_i())
+  erb(:ingredient_info)
+end
+
+patch("/ingredients/:id") do
+  ingredient_id = params.fetch("id").to_i()
+  @ingredient = Ingredient.find(ingredient_id)
+  recipe_ids = params.fetch("recipe_ids")
+  @ingredient.update({:recipe_ids => recipe_ids})
+  @recipes = Recipe.all()
+  erb(:ingredient_info)
+end
