2a1a9f5376979bdc3332d0fd17f36b932c49f8d1
Jackie Fletcher & Lauren Marcus <student@epicodus.com>
2015-01-29 10:59:49 -0800

    Ensure that the ingredient form isn't blank.

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -2,4 +2,8 @@ require('spec_helper')
 
 describe(Ingredient) do
   it { should have_and_belong_to_many (:recipes) }
+  it("validates presence of food") do
+    ingredient = Ingredient.new({:food => ''})
+    expect(ingredient.save()).to(eq(false))
+  end
 end
